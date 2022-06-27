3b1b47fd2ac00921752e58cf6dfde53f1383e914
Nicolas TAPIA <ntapia.dev@gmail.com>
2022-06-27 16:55:48 +0200

    Test expo

--- App.tsx
+++ App.tsx
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { Separator } from './components/Separator';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.headerStyle}
+      >
+        Yop!
+      </Text>
+      <Separator color='blue' />
+      <TouchableOpacity>
+        <Image
+          style={{ width: 200, height: 300 }}
+          source={{
+            uri: "https://www.santevet.com/upload/admin/images/article/Chat%202/portrait_chat/les_5_sens_du_chat.jpg"
+          }}
+        />
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +30,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  headerStyle: {
+    fontSize: 30,
+    fontWeight: "bold"
+  }
 });
